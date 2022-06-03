eacee2d609da23637d3999b524909140a8f69b24
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-06-03 09:05:22 +0800

    [feature] change to topic force insert method

--- internal/repository/topic.go
+++ internal/repository/topic.go
@@ -9,6 +9,7 @@ import (
 
 func (r *repository) MigrateTopics(topics []model.Topic) (err error) {
 	var topicSpecs []kafka.TopicSpecification
+	var topicNames []string
 
 	for _, v := range topics {
 		topicSpecs = append(topicSpecs, kafka.TopicSpecification{
@@ -16,9 +17,17 @@ func (r *repository) MigrateTopics(topics []model.Topic) (err error) {
 			NumPartitions:     v.Partition,
 			ReplicationFactor: v.ReplicationFactor,
 		})
+		topicNames = append(topicNames, v.Name)
 	}
 
-	res, err := r.kafkaAdmin.CreateTopics(context.Background(),
+	res, err := r.kafkaAdmin.DeleteTopics(context.Background(), topicNames)
+	if err != nil {
+		r.logger.Errorf("error deleting topics: %+v", err)
+		return
+	}
+	r.logger.Infof("delete topics response: %+v", res)
+
+	res, err = r.kafkaAdmin.CreateTopics(context.Background(),
 		topicSpecs,
 	)
 	if err != nil {
